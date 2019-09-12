46d1a3c56b312078b6ca774389df7a236c780083
Aydar Farrakhov <aydar.farrakhov@akvelon.com>
2019-09-12 22:55:59 +0300

    responsive layout

---
+++ twitter-like-client/src/app/shared/photo-preview/photo-preview.component.ts
@@ -0,0 +1,16 @@
+import { Component, Inject, OnInit } from '@angular/core'
+import { MAT_DIALOG_DATA } from '@angular/material'
+
+@Component({
+  selector: 'app-photo-preview',
+  templateUrl: './photo-preview.component.html',
+  styleUrls: ['./photo-preview.component.scss'],
+})
+export class PhotoPreviewComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: string) {
+  }
+
+  ngOnInit() {
+  }
+}
